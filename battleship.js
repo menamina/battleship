4064e5d354eac103b751c07652aea53b0dcbb252
Mena <dayjacs03@gmail.com>
2025-10-07 19:06:56 -0500

    adding logic for the gamebaord function

--- battleship.js
+++ battleship.js
@@ -32,7 +32,41 @@ function Ship(size) {
 }
 
 function Gameboard(){
+    const board = [
+    [null, null, null, null, null, null, null, null, null, null], // row 0
+    [null, null, null, null, null, null, null, null, null, null], // row 1
+    [null, null, null, null, null, null, null, null, null, null], // row 2
+    [null, null, null, null, null, null, null, null, null, null], // row 3
+    [null, null, null, null, null, null, null, null, null, null], // row 4
+    [null, null, null, null, null, null, null, null, null, null], // row 5
+    [null, null, null, null, null, null, null, null, null, null], // row 6
+    [null, null, null, null, null, null, null, null, null, null], // row 7
+    [null, null, null, null, null, null, null, null, null, null], // row 8
+    [null, null, null, null, null, null, null, null, null, null]  // row 9
+    ];
 
+    return {
+        board,
+
+        placeShip(shipLength, x, y, direction = null) {
+        if (direction === 'horizontal' && x + shipLength > 10) {
+            throw new Error("Ship does not fit horizontally");
+        }
+        if (direction === 'vertical' && y + shipLength > 10) {
+            throw new Error("Ship does not fit vertically");
+        }
+
+        const newShip = Ship(shipLength);
+
+        for (let i = 0; i < shipLength; i++) {
+            if (direction === 'horizontal') {
+            board[y][x + i] = newShip; // row = y, column = x+i
+            } else {
+            board[y + i][x] = newShip; // row = y+i, column = x
+            }
+        }
+        }
+    }
 }
 
 module.exports = {Ship, Gameboard};
